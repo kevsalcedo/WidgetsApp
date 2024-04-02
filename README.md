4af22c347ea91dd922f9036a45fb69e2b7107f24
kevsalcedo <kevsalcedom@gmail.com>
2024-04-02 18:51:22 -0500

    add gif and readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # widgets_app
 
-A new Flutter project.
+## Description
+
+Application created to test a large number of widgets and their implementation with Material 3. We also implemented the use of Riverpod for the global states of the application. 
+
+## Examples
+
+![Example](assets/gifts/widgetgif.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,15 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final bool isDarkMode = ref.watch(isDarkModeProvider);
-    final int selectedColor = ref.watch(selectedColorProvider);
+    //final bool isDarkMode = ref.watch(isDarkModeProvider);
+    //final int selectedColor = ref.watch(selectedColorProvider);
+    final AppTheme appTheme = ref.watch(themeNotifierProvider);
 
     return MaterialApp.router(
       title: 'Flutter Widgets',
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: selectedColor, isDarkMode: isDarkMode).getTheme(),
+      theme: appTheme.getTheme(),
     );
   }
 }
